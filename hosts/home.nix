0cba6f198c7ba2cbc896ffc1ee150e9d9400c3d1
ollieistic <135356998+ollieistic@users.noreply.github.com>
2025-10-30 16:40:24 +0100

    Add gtk, xdg, theme configurations and move user-level packages to shared home.nix.

--- hosts/home.nix
+++ hosts/home.nix
@@ -1,12 +1,71 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
-   # ../user
+    #../user
   ];
 
   home.username = "ollie";
   home.homeDirectory = "/home/ollie";
   home.stateVersion = "25.05";
+
+  home.pointerCursor = {
+    x11.enable = true;
+    gtk.enable = true;
+    package = pkgs.capitaine-cursors;
+    size = 24;
+    name = "Capitaine-Cursors";
+  };
+
+  gtk = {
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      music = "${config.home.homeDirectory}/Media/Music";
+      videos = "${config.home.homeDirectory}/Media/Videos";
+      publicShare = null;
+      templates = null;
+    };
+  };
+
+  home.packages = with pkgs; [
+    librewolf
+    ungoogled-chromium
+    brave
+    vscode
+    heroic
+    hyprshot
+    vesktop
+    bitwarden-desktop
+    opencode
+    fastfetch
+    cava
+    tty-clock
+    waypaper
+    swww
+    spotify
+    rofi
+    localsend
+  ];
 }
 
