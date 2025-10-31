c33ee2c4de75f8d7cf222ab3719eb3d4cd28be8b
ollieistic <135356998+ollieistic@users.noreply.github.com>
2025-10-31 14:42:55 +0100

    Switch to unstable branch for all packages

--- flake.nix
+++ flake.nix
@@ -2,31 +2,25 @@
   description = "Ollieistic's Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     silentSDDM.url = "github:uiriansan/SilentSDDM";
     silentSDDM.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, unstable, home-manager, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
-
-    unstable-pkgs = import unstable {
-      inherit system;
-      config.allowUnfree = true;
-    };
   in
   {
     nixosConfigurations = {
       home = inputs.nixpkgs.lib.nixosSystem {
         inherit system;
-
-        specialArgs = { unstable = unstable-pkgs; inherit inputs; };
       
         modules = [
           ./hosts/home/configuration.nix
